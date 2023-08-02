4d89c3116e06d792e32df0e0bd05d4496291088b
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-02 16:43:42 -0400

    Create basic DOM visuals

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 // ===== QUERY SELECTORS =====
+var userInputArea = document.querySelector("#userInput");
+var gamePlayArea = document.querySelector("#gamePlay");
 
 // ===== DATA MODEL =====
+var gameIcons = [
+  [
+    {
+      name: "rock",
+      img: "happy-rock.png",
+    },
+    {
+      name: "paper",
+      img: "happy-paper.png",
+    },
+    {
+      name: "scissors",
+      img: "happy-scissors.png",
+    },
+  ],
+];
+
 var players = [];
 var easy = ["rock", "paper", "scissors"];
 var currentGame;
 
 // ===== EVENT LISTENERS =====
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+  var player1 = createPlayer("Humanoid", "🤖");
+  var player2 = createPlayer("Computer", "💻");
+
+  // var score = checkWin(game);
+  // var game = createGame([player1, player2], easy);
+  // var score = checkWin(game);
+  // var game = createGame([player1, player2], easy);
+  // var score = checkWin(game);
+});
+
+userInputArea.addEventListener("click", function (event) {
+  createGame(players, event);
+  buildBoard(currentGame, event);
+});
+
+gamePlayArea.addEventListener("click", function (event) {
+  takeTurn(currentGame, event);
+});
+
+// gamePlayArea.addEventListener()
 
 // ===== FUNCTIONS =====
 
@@ -16,22 +55,41 @@ function createPlayer(name, token) {
     token: token,
     wins: 0,
   };
+  players.push(player);
   return player;
 }
 
-function createGame(players, level) {
+function createGame(players, event) {
   // for (let i = 0; i < players.length; i++) {
   //   players[i].selection = selections[i];
   // }
+  var level;
+  var levelSelection = event.target.closest(".select-game").id;
+  if (levelSelection === "easy") {
+    level = easy;
+  } else {
+    // PUT HARD HERE LATER
+  }
   var selections = [];
-  var currentGame = {
+  currentGame = {
     players: players,
     selections: selections,
     level: level,
   };
+  console.log(currentGame);
+  return currentGame;
+}
+
+function takeTurn(status, event) {
+  if (status === "human") {
+    var fighterSelection = event.target.closest(".fighter").id;
+    console.log(fighterSelection);
+  } else {
+    console.log("human response");
+  }
   for (let i = 0; i < players.length; i++) {
-    var input = prompt(`Please make your selection ${level}.`);
-    console.log(`${players[i].name} has chosen ${input}`);
+    // var input = prompt(`Please make your selection ${level}.`);
+    // console.log(`${players[i].name} has chosen ${input}`);
     selections.push(input);
   }
   console.log(currentGame);
@@ -86,11 +144,15 @@ function checkDraw(game) {
 
 function reset() {}
 
-var player1 = createPlayer("Humanoid", "🤖");
-var player2 = createPlayer("Computer", "💻");
-var game = createGame([player1, player2], easy);
-var score = checkWin(game);
-var game = createGame([player1, player2], easy);
-var score = checkWin(game);
-var game = createGame([player1, player2], easy);
-var score = checkWin(game);
+function buildBoard(game) {
+  userInputArea.classList.toggle("hidden", true);
+  console.log(userInputArea);
+  gamePlayArea.innerHTML = "";
+  for (let i = 0; i < game.level.length; i++) {
+    gamePlayArea.innerHTML += `
+  <div id="${game.level[i]}" class="fighter cursor">
+    <img class="fighter-avatar" src="assets/happy-${game.level[i]}.png" alt="">
+    <div class="hidden mini-emoji">Emoji here</div>
+  </div>`;
+  }
+}
