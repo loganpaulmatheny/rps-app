99bd12a1fd4838f9d4856796b81bd05c17127210
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-03 17:47:12 -0400

    Complete all DOM updates for easy version

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // ===== QUERY SELECTORS =====
+var gameMessage = document.querySelector("#gameMessage");
 var userInputArea = document.querySelector("#userInput");
 var gamePlayArea = document.querySelector("#gamePlay");
+var player1Score = document.querySelector("#score1");
+var player2Score = document.querySelector("#score2");
+var changeGameButton = document.querySelector("#changeGame");
 
 // ===== DATA MODEL =====
 var gameIcons = [
@@ -50,6 +54,8 @@ gamePlayArea.addEventListener("click", function (event) {
   handleSelections(event);
 });
 
+changeGameButton.addEventListener("click", changeGames);
+
 // gamePlayArea.addEventListener()
 
 // ===== FUNCTIONS =====
@@ -68,8 +74,9 @@ function createGame(players, round, event) {
   // for (let i = 0; i < players.length; i++) {
   //   players[i].selection = selections[i];
   // }
+  gameMessage.innerText = "Choose your fighter!";
+  var level;
   if (round === 0) {
-    var level;
     var levelSelection = event.target.closest(".select-game").id;
     if (levelSelection === "easy") {
       level = easy;
@@ -97,21 +104,21 @@ function takeTurn(event) {
   // console.log(playerUp);
   if (playerUp === "Humanoid") {
     var fighterSelection = event.target.closest(".fighter").id;
-    console.log(fighterSelection);
+    // console.log(fighterSelection);
     currentGame.selections.push(fighterSelection);
     // console.log(currentGame);
     var selectedBy = event.target.closest(".fighter");
     selectedBy.innerHTML += `<div class="mini-emoji">${currentGame.players[playerTurn].token}</div>`;
   } else {
     var randomFighter = randomSelection();
-    console.log(randomFighter);
+    // console.log(randomFighter);
     currentGame.selections.push(randomFighter);
     var selectedBy = document.getElementById(`${randomFighter}`);
-    console.log(selectedBy);
+    // console.log(selectedBy);
     selectedBy.innerHTML += `<div class="mini-emoji">${currentGame.players[playerTurn].token}</div>`;
   }
   playerTurn += 1;
-  console.log(currentGame);
+  // console.log(currentGame);
   return playerTurn;
   // DISPLAY WHO SELECTED ITEM HERE
   // PAUSE for 3 seconds
@@ -129,35 +136,60 @@ function checkWin(game) {
   // case whens?
   // only one circumstance where each wins -- if statements fine
   // pull out the selections compare them
+
+  // ======= WHAT LEVEL DO WE NEED TO CHECK ======
   var winner;
-  console.log(game);
+  // console.log(game);
   var draw = checkDraw(game);
   if (draw) {
-    console.log(`It's a draw!`);
+    gameMessage.innerText = `It's a draw!`;
   } else {
-    if (
-      game.selections.includes("rock") &&
-      game.selections.includes("scissors")
-    ) {
-      winner = game.selections.indexOf("rock");
-    } else if (
-      game.selections.includes("paper") &&
-      game.selections.includes("rock")
-    ) {
-      console.log(game.selections);
-      winner = game.selections.indexOf("paper");
-    } else if (
-      game.selections.includes("scissors") &&
-      game.selections.includes("paper")
-    ) {
-      winner = game.selections.indexOf("scissors");
+    // if (
+    //   game.selections.includes("rock") &&
+    //   game.selections.includes("scissors")
+    // ) {
+    //   winner = game.selections.indexOf("rock");
+    // } else if (
+    //   game.selections.includes("paper") &&
+    //   game.selections.includes("rock")
+    // ) {
+    //   winner = game.selections.indexOf("paper");
+    // } else if (
+    //   game.selections.includes("scissors") &&
+    //   game.selections.includes("paper")
+    // ) {
+    //   winner = game.selections.indexOf("scissors");
+    // }
+    // console.log(winner);
+    if (game.level === easy) {
+      var winner = easyLogic(game);
+    } else if (game.level === hard) {
+      // PUT HARD LOGIC FUNCTION HERE
     }
-    console.log(winner);
     game.players[winner].wins += 1;
-    console.log(`The winner is the ${game.players[winner].name}!`);
-    console.log(players);
-    round += 1;
+    gameMessage.innerText = `The winner is the ${game.players[winner].name}!`;
   }
+  round += 1;
+}
+
+function easyLogic(game) {
+  if (
+    game.selections.includes("rock") &&
+    game.selections.includes("scissors")
+  ) {
+    winner = game.selections.indexOf("rock");
+  } else if (
+    game.selections.includes("paper") &&
+    game.selections.includes("rock")
+  ) {
+    winner = game.selections.indexOf("paper");
+  } else if (
+    game.selections.includes("scissors") &&
+    game.selections.includes("paper")
+  ) {
+    winner = game.selections.indexOf("scissors");
+  }
+  return winner;
 }
 
 function checkDraw(game) {
@@ -177,7 +209,7 @@ function handleSelections(event) {
       resolve(
         setTimeout(() => {
           takeTurn();
-        }, 3000)
+        }, 2000)
       );
     } else {
       reject("Error");
@@ -188,14 +220,43 @@ function handleSelections(event) {
       prepResults;
     })
     .then(function () {
-      setTimeout(() => checkWin(currentGame), 4000);
+      setTimeout(() => checkWin(currentGame), 3000);
     })
     .then(function () {
-      setTimeout(() => reset(), 4500);
+      setTimeout(() => updateScoreboards(), 3000);
+    })
+    .then(function () {
+      setTimeout(() => reset(), 5000);
     })
     .catch((prepResults) => {
       prepResults;
     });
+}
+
+function buildBoard(game) {
+  showElement(gamePlayArea);
+  hideElement(userInputArea);
+  showElement(changeGameButton);
+  changeGameButton.classList.toggle("hidden", false);
+  // console.log(userInputArea);
+  gamePlayArea.innerHTML = "";
+  for (let i = 0; i < game.level.length; i++) {
+    gamePlayArea.innerHTML += `
+    <div id="${game.level[i]}" class="fighter cursor">
+    <img class="fighter-avatar" src="assets/happy-${game.level[i]}.png" alt="">
+    </div>`;
+  }
+}
+
+function updateScoreboards() {
+  player1Score.innerText = `Wins - ${players[0].wins}`;
+  player2Score.innerText = `Wins - ${players[1].wins}`;
+}
+
+function changeGames() {
+  console.log("clicked");
+  showElement(userInputArea);
+  hideElement(gamePlayArea);
 }
 
 function reset() {
@@ -204,14 +265,10 @@ function reset() {
   createGame(currentGame, round);
 }
 
-function buildBoard(game) {
-  userInputArea.classList.toggle("hidden", true);
-  console.log(userInputArea);
-  gamePlayArea.innerHTML = "";
-  for (let i = 0; i < game.level.length; i++) {
-    gamePlayArea.innerHTML += `
-  <div id="${game.level[i]}" class="fighter cursor">
-    <img class="fighter-avatar" src="assets/happy-${game.level[i]}.png" alt="">
-  </div>`;
-  }
+function hideElement(selector) {
+  selector.classList.toggle("hidden", true);
+}
+
+function showElement(selector) {
+  selector.classList.toggle("hidden", false);
 }
